d644f60ffa7ee86ebf88fb857ad78acca0633e77
BaukeWestendorp <baukewestendorp@gmail.com>
2022-12-13 00:05:29 +0100

    nvim: Remove undo file

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,9 +7,6 @@ vim.opt.smartindent = true
 vim.opt.relativenumber = true
 vim.opt.nu = true
 
-vim.opt.undodir = '~/.vim/undodir'
-vim.opt.undofile = true
-
 vim.opt.incsearch = true
 
 vim.opt.scrolloff=8
