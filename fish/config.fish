cb9f33f0f238a9c963424bafbfd9addcea86364c
Bauke Westendorp <baukewestendorp@gmail.com>
2024-01-09 16:04:11 +0100

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if not status --is-interactive
+end
+
+abbr -a gap git add -p
+abbr -a ga. git add .
+abbr -a gca git commit --amend
+abbr -a gc git commit
+abbr -a gd git diff
+abbr -a gs git status
+abbr -a gl git log
+abbr -a gco git checkout
+abbr -a gu gitui
+abbr -a gl1 git log --oneline
